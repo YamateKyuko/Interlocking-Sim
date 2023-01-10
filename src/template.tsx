c671517dfb4eaa2e891a0a7bb4134e1bb3043fc4
swallow3 <akakiryou@icloud.com>
2023-01-10 15:41:12 +0900

    z

---
+++ src/template.tsx
@@ -0,0 +1,57 @@
+export type template_data = {
+  "Destinations": {
+    "1":{
+      "canLever": number[]
+    },
+    "2":{
+      "canLever": number[]
+    },
+    "3":{
+      "canLever": number[]
+    },
+    "4":{
+      "canLever": number[]
+    },
+    "5":{
+      "canLever": number[]
+    },
+    "6":{
+      "canLever": number[]
+    },
+    "7":{
+      "canLever": number[]
+    },
+    "8":{
+      "canLever": number[]
+    }
+  }
+}
+
+export type template = {
+  "Destinations": {
+    "1":{
+      "canLever": number[]
+    },
+    "2":{
+      "canLever": number[]
+    },
+    "3":{
+      "canLever": number[]
+    },
+    "4":{
+      "canLever": number[]
+    },
+    "5":{
+      "canLever": number[]
+    },
+    "6":{
+      "canLever": number[]
+    },
+    "7":{
+      "canLever": number[]
+    },
+    "8":{
+      "canLever": number[]
+    }
+  }
+};
